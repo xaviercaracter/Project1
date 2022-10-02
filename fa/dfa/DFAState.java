b560799f2247eccf53246c362359954c4bc99b39
xaviercaracter <85382381+xaviercaracter@users.noreply.github.com>
2022-10-01 21:58:06 -0600

    Last Commit for DFAState

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -4,9 +4,20 @@ import fa.State;
 
 import java.util.HashMap;
 
+/**
+ * Class that represents a DFAState
+ * Each State uses a hashmap for its transitions to other states
+ * on a given transition symbol
+ *
+ * @author Xavier Caracter
+ */
+
 public class DFAState extends State {
 
     //Fields
+    /** HashMap that stores the DFAState's transitions Character is the transition char
+     * DFAState is the destination state
+     */
     private HashMap<Character, DFAState> trans;
 
     //Constructor
@@ -21,10 +32,20 @@ public class DFAState extends State {
         return name;
     }
 
+    /**
+     * Puts the correct target state in the hashmap of transition states
+     * @param tranSymb char transition symbol
+     * @param toState Destination DFAState
+     */
     public void toState(char tranSymb, DFAState toState){
         trans.put(tranSymb, toState);
     }
 
+    /**
+     * Retruns the correct DFAState according to the transition symbol
+     * @param toSymb char transition symbol
+     * @return DFAState we are looking for according to toSymb
+     */
     public DFAState getToState(char toSymb){
         return trans.get(toSymb);
     }
